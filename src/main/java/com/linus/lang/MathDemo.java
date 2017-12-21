af88d4e1afd6862121ca9a1407bd14997dcf782e
Linus Yan <lyan2@ebay.com>
2017-12-21 08:38:34 +0800

    Add Math Demo

---
+++ src/main/java/com/linus/lang/MathDemo.java
@@ -0,0 +1,12 @@
+package com.linus.lang;
+
+public class MathDemo {
+
+	public static void main(String[] args) {
+		double d = 3.456;
+		System.out.println(Math.floor(d));
+		System.out.println(Math.floor(d * 100) / 100);
+		System.out.println(Math.round(d));
+	}
+
+}
